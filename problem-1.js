b51a73551cf38ed6cf7d8bfe079def57b5f21d42
mdsharia4613 <mdsharia4613@gmail.com>
2025-10-07 22:19:24 +0600

    provlem solving

---
+++ problem-1.js
@@ -0,0 +1,7 @@
+
+const count = 'Hello';
+let result = 0;
+for(let char of count){
+    result++;
+}
+console.log(result)
---
+++ provlem-2.js
@@ -0,0 +1,10 @@
+
+const text = 'world';
+let count = '';
+for(let i = text.length -1; i >= 0; i-- ){
+    count += text[i];
+}
+console.log('Reversed string:' ,count)
+
+// Count vowels in a string
+
