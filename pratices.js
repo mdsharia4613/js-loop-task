5f51a17136f1c15da273b76ac9d5f561e5f43502
mdsharia4613 <mdsharia4613@gmail.com>
2025-10-07 23:46:15 +0600

    provlem-2

--- pratices.js
+++ pratices.js
@@ -11,10 +11,32 @@ console.log('Reversed string:', count)
 
 const value = 'samim haque';
 let counts = 0;
-for( i = 0; i < value.length; i++){
+for(let i = 0; i < value.length; i++){
     const char = value[i].toLowerCase();
     if(char == 'a' || char == 'e' || char == 'i' || char == 'o' || char == 'u'){
         counts++;
     }
 }
 console.log('vawel' ,counts);
+
+// Find the largest number in an array
+
+const number = [12, 10, 9, 3, 22, 2];
+let values = number[0];
+for( i = 0; i < number.length; i ++){
+    if( number[i] > values){
+        values = number[i];
+    }
+}
+console.log('max', values);
+
+// Sum of digits of a number
+
+const num = 1234;
+let sum = 0;
+const str = num.toString();
+for(let i = 0; i < str.length; i++){
+    sum = sum + parseInt(str[i]);
+}
+
+console.log('sum', sum);
