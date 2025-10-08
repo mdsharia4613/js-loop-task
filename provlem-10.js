a8b9a56cb18d8496f78d027ac9c00940cd84f9a6
mdsharia4613 <mdsharia4613@gmail.com>
2025-10-08 11:19:19 +0600

    provlem-10,11

---
+++ provlem-10.js
@@ -0,0 +1,7 @@
+
+// Repeat Word Using Loop
+
+const text = 'Hello';
+for( i = 0; i < 4; i++){
+    console.log(text);
+}
---
+++ provlem-11.js
@@ -0,0 +1,5 @@
+
+// Remove Extra Spaces
+
+const sentences = "hello js ";
+console.log(sentences.trim().replaceAll(" ", " "));
